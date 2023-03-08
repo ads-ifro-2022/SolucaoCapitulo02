8c8dd4774dba5965032ec5208df48c75049a22fc
adsifro <ads_ifro_2022@outlook.com>
2023-03-07 20:17:48 -0400

    foi adiciona um evento ao botão

--- CalculaIdade/Form1.cs
+++ CalculaIdade/Form1.cs
@@ -16,5 +16,10 @@ namespace CalculaIdade
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
